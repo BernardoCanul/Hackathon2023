14f928630841f3ff3968ea4d956143017b4b8915
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-05-27 02:58:52 -0600

    make login containers

--- src/loging.py
+++ src/loging.py
@@ -1,8 +1,40 @@
 import flet as ft
 
+HEIGHT = 650
+WIDTH = 615
+
 class LogIn:
     def __init__(self, page: ft.Page):
         self.page = page
-        output_text = ft.Text("HOLA", size=32, text_align="start")
-        self.page.add(output_text)
+        main_container, extra_contanier = self.loginContainer()
+        self.page.add(ft.Row([main_container, extra_contanier]))
         self.page.update()
+
+    def loginContainer(self):
+        login = ft.Container(
+            width=WIDTH,
+            height=HEIGHT,
+            border_radius=35,
+            padding=5,
+            # bgcolor="green"
+            gradient=ft.LinearGradient(
+                begin=ft.alignment.top_right,
+                end=ft.alignment.bottom_right,
+                colors=['brown']
+                )
+        )
+
+        extra = ft.Container(
+            width=WIDTH,
+            height=HEIGHT,
+            border_radius=35,
+            padding=5,
+            # bgcolor="green"
+            gradient=ft.LinearGradient(
+                begin=ft.alignment.top_left,
+                end=ft.alignment.bottom_left,
+                colors=['green']
+                )
+        )
+
+        return login, extra
