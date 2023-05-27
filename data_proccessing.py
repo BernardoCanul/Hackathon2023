1a4e0e385e82a1c1c9c7558b21e531e9f6b10d58
BernardoCanul <bcanul01@outlook.es>
2023-05-27 09:52:32 -0600

    Dataset added

---
+++ data_proccessing.py
@@ -0,0 +1,35 @@
+import os
+import shutil
+
+root_dir = "garbage_classification"
+split_ratio = 0.8
+
+for type in os.listdir(root_dir):
+    class_path = os.path.join(root_dir, type)
+
+    # Calculate ratio of train images and mve to new path
+    train_samples = int(len(os.listdir(class_path)) * split_ratio)
+    path = os.path.join("img", "train", type)
+    os.mkdir(path)
+
+    count = 0
+    for img in os.listdir(class_path):
+        if count <= train_samples:
+            src = os.path.join(class_path, img)
+            dst = os.path.join(path, img)
+                
+            shutil.move(src, dst)
+            count += 1
+
+    # Move the rest of the images to test
+    path = os.path.join("img", "test", type)
+    os.mkdir(path)
+    for img in os.listdir(class_path):
+        src = os.path.join(class_path, img)
+        dst = os.path.join(path, img)
+
+        shutil.move(src, dst)
+
+        
+
+        
